2dc4981283d5feaf9b7cc943ac450919b155e3f1
anirudhn <anirudhn@lucid-green.com>
2021-12-03 17:19:52 +0530

    Auth apis merge

--- src/main/java/com/example/demo/model/Role.java
+++ src/main/java/com/example/demo/model/Role.java
@@ -5,25 +5,26 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "Role")
 public class Role {
-	
+
 	@Id
 	private String id;
 	private String name;
-	
-	public Role() {}
-	
+
+	public Role() {
+	}
+
 	public Role(String rolename) {
-		this.name=rolename;
+		this.name = rolename;
 	}
 
 	public String getId() {
 		return id;
 	}
-	
+
 	public void setId(String id) {
-		this.id=id;
+		this.id = id;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
